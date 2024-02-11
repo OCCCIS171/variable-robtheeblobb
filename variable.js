0f6073c0e02bed481a5bdca946b43c8aabc25049
Robert Simpson <159501264+robtheeblobb@users.noreply.github.com>
2024-02-11 09:22:29 -0800

    Update variable.js
    Signed-off-by: Robert Simpson <159501264+robtheeblobb@users.noreply.github.com>

--- variable.js
+++ variable.js
@@ -1,9 +1,11 @@
 // TODO: Create a variable named `firstName` and assign your first name to it in the line below.
-let firstName = Robert
+let firstName = "Robert";
 // TODO: Create a variable named `lastName` and assign your last name to it in the line below.
-let lastName = Simpson
+let lastName = "Simpson";
 
-let sayHello = "Hello world! My name is <firstName> <lastName> . Nice to meet you!"
+let sayHello = "Hello world! My name is " + firstName + " " + lastName + ". Nice to meet you!"
+
+console.log(sayHello)
 
 // TODO: Create a variable named `sayHello` and build the following string based on the variables
 // above: "Hello world! My name is <firstName> <lastName>. Nice to meet you!". 
